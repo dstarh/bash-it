61c5ef91901e7a65fe0aab97343833372ad5b002
David Herman <dstarh@purpleasphalt.com>
2012-11-22 08:39:03 -0500

    cleanup

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -1,9 +1,6 @@
 alias undopush="push -f origin HEAD^:master"
 alias undocommit="git reset --soft HEAD^"
-alias am='cd ~/workspace/reflex-am'
-alias amweb='cd ~/workspace/reflex-am-web'
 alias l.='ls -dl .*'
 alias breset='. ~/.bash_profile'
-#alias mdj='mvnDebug jetty:run'
 alias gpod='git push origin develop'
 alias gffc='git flow feature checkout'
